8ae1ae59fe36a9a9a03171cb88b855058136f9ca
Arpit Mishra <106508210+arpitmiahra4@users.noreply.github.com>
2023-04-25 13:29:18 +0530

    adding: usefull assets and page to go on now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Box } from '@chakra-ui/react';
 import './App.css';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <Box>
+    <LandingPage />
+    </Box>
   );
 }
 
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <Box>LandingPage</Box>
+  )
+}
+
+export default LandingPage
